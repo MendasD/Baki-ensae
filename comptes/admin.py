bc0f07650b313c4945867ee5a68d6d6ac82dd17d
MendasD <christnzonde@gmail.com>
2024-10-18 11:17:20 +0000

    modification de l interface admin pour les autres tables

--- comptes/admin.py
+++ comptes/admin.py
@@ -7,9 +7,15 @@ from .models import Utilisateur, Sujets, Corriges, Documents
 class SujetsAdmin(admin.ModelAdmin):
     list_display = ('ecole', 'classe', 'semestre','matiere','titre','annee')
 
+class DocumentsAdmin(admin.ModelAdmin):
+    list_display = ('matiere', 'classe', 'type','description','fichier')
+
+class CorrigesAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'date','description')
+
 
 # Pour enregistrer des modèles sur l'interface admin de django
 admin.site.register(Utilisateur)
 admin.site.register(Sujets,SujetsAdmin)
-admin.site.register(Corriges)
-admin.site.register(Documents)
+admin.site.register(Corriges,CorrigesAdmin)
+admin.site.register(Documents,DocumentsAdmin)
