7efc9381853cd5a4bbcb1e0b2c8df53c95a3d5c6
MendasD <christnzonde@gmail.com>
2024-10-18 10:49:33 +0000

    modification de l interface admin pour la table Sujets

--- comptes/admin.py
+++ comptes/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
+from .models import Utilisateur, Sujets, Corriges, Documents
+
 
 # Register your models here.
 
-from django.contrib import admin
-from django.contrib import admin
-# from Library.models import Document, Vente
-from .models import Utilisateur, Sujets, Corriges, Documents
+class SujetsAdmin(admin.ModelAdmin):
+    list_display = ('ecole', 'classe', 'semestre','matiere','titre','annee')
 
 
 # Pour enregistrer des modèles sur l'interface admin de django
 admin.site.register(Utilisateur)
-admin.site.register(Sujets)
+admin.site.register(Sujets,SujetsAdmin)
 admin.site.register(Corriges)
 admin.site.register(Documents)
